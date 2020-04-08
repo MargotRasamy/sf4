e42d0c2c14e986aa53f95f8fdb4cc9a78cdc0391
Margot Rasamy <rasamymargot@gmail.com>
2020-04-08 11:51:17 +0200

    Chapter 2 - Symfony course

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,20 +1,22 @@
 <?php
 
 namespace App\Controller;
-
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class HomeController extends AbstractController
 {
+//    La route correspondante à l'action index()
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="homepage")
      */
-    public function index()
+    public function index(Request $request, Session $session)
     {
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/HomeController.php',
+            'test' => $request->query->get('foo'),
+            'session' => $session->has('cle_inexistante')
         ]);
     }
 }
