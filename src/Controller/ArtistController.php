d9408aff165325b0f73e31deb42a5f467ff90caf
Margot Rasamy <rasamymargot@gmail.com>
2020-04-08 22:12:26 +0200

    update

--- src/Controller/ArtistController.php
+++ src/Controller/ArtistController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ArtistController extends AbstractController
 {
@@ -12,6 +13,30 @@ class ArtistController extends AbstractController
      */
     public function index()
     {
-        return $this->render('artists/list.html.twig');
+        return $this->render('artists/list.html.twig', [
+            'ArtistName' => 'Your favorite artists name'
+        ]);
     }
+
+
 }
+
+//on peut aussi faire comme ca
+
+///**
+// * @Route("/artist", name="artist_")
+// */
+//class ArtistController extends AbstractController
+//{
+//    /**
+//     * URI:/artist-list
+//     * Nom:artist_list
+//     * @Route("/-list", name="list")
+//     */
+//    public function index()
+//    {
+//        return $this->render('artists/list.html.twig', [
+//            'ArtistName' => 'Beyonce'
+//        ]);
+//    }
+//}
